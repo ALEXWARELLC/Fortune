af64ccf94bd4ba999f4e39946aff6bfb725a9ba1
ɅLΞX <123724383+ALEXWARELLC@users.noreply.github.com>
2023-11-17 16:50:02 +0800

    Plugin Functional.

--- Config.cs
+++ Config.cs
@@ -1,9 +1,19 @@
 using Exiled.API.Interfaces;
+using System.ComponentModel;
 
 namespace Fortune;
 
 public class Config : IConfig
 {
+    [Description("Is the Fortune Flip plugin enabled?")]
     public bool IsEnabled { get; set; }
+
+    [Description("This does nothing. No need to activate this.")]
     public bool Debug { get; set; }
+
+    [Description("Maximum range for the random number generator.")]
+    public int ActivationMax { get; set; } = 10;
+
+    [Description("The maximum number required to apply random effect.")]
+    public int ActivationChance { get; set; } = 3;
 }
--- Plugin.cs
+++ Plugin.cs
@@ -1,8 +1,51 @@
-﻿using Exiled.API.Features;
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
 
 namespace Fortune;
 
 public class Plugin : Plugin<Config>
 {
-    
+    public override string Name => "Fortune Flip";
+    public override string Author => "ALEXWARELLC & Zachuttak0";
+    public override string Prefix => "fortuneflip";
+    public override bool IgnoreRequiredVersionCheck => true;
+
+    public override void OnEnabled()
+    {
+        Log.Info($"Registering {Name} events...");
+
+        Exiled.Events.Handlers.Player.FlippingCoin += OnUsingCoin;
+    }
+
+    public override void OnDisabled()
+    {
+        Log.Info($"Unregistering {Name} events...");
+
+        Exiled.Events.Handlers.Player.FlippingCoin -= OnUsingCoin;
+    }
+
+    public void OnUsingCoin(FlippingCoinEventArgs ev)
+    {
+        if (!Config.IsEnabled)
+            return;
+
+        if (new Random().Next(0, Config.ActivationMax) <= Config.ActivationChance)
+        {
+            if (ev.IsTails)
+            {
+                EffectType effect = ev.Player.ApplyRandomEffect(Exiled.API.Enums.EffectCategory.Harmful, 20, true);
+                ev.Player.Broadcast(5, $"You received the {effect} effect.");
+            }
+            else
+            {
+                EffectType effect = ev.Player.ApplyRandomEffect(Exiled.API.Enums.EffectCategory.Positive, 20, true);
+                ev.Player.Broadcast(5, $"You received the {effect} effect.");
+            }
+        }
+        else
+        {
+            ev.Player.Broadcast(5, "You didn't receive an effect.");
+        }
+    }
 }
